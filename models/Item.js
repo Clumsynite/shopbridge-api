a563f4ae5483ae4b6be72307a50ab3b85420d4af
Rishabh Pathak <clusmsyknight@gmail.com>
2022-03-17 15:49:07 +0530

    added description and price in item model

--- models/Item.js
+++ models/Item.js
@@ -6,6 +6,8 @@ const itemSchema = new Schema(
   {
     added: { type: Date, default: Date.now },
     name: { type: String, required: true },
+    description: { type: String, required: true },
+    price: { type: Number, required: true },
     quantity: { type: Number, required: true },
     photo: { type: String, required: false },
     updated: { type: Date, default: Date.now },
